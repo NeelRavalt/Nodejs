c15e39bb01d3f529b6a9310acb04bb65c3186dfb
Neel <neelraval16113@gmail.com>
2023-08-31 11:44:31 +0530

    APIwork

---
+++ fetchAPI/fetch.js
@@ -0,0 +1,19 @@
+//api + api key copy pate kari ne {} fill karo
+
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=70da3369b09d3c18e9555ec67caf378e";
+
+
+fetch(URL,{
+    method: "GET",
+})
+// response and result ne .then ma use karvu.
+//data ne lavva mate promise ma .then 2 var lagavu pade.
+.then((response)=>{
+    return response.json();
+    //return response.text();
+})
+
+
+.then((result)=>{
+    console.log(result)
+})
